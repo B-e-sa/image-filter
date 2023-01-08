743e93930a7489534116b41be498ed0370e2def0
B-e-sa <joaospereira963@gmail.com>
2023-01-08 18:19:22 -0300

    add grayscale

--- helpers.c
+++ helpers.c
@@ -3,6 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int rgbt = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = rgbt;
+        }
+    }
     return;
 }
 
